c6535b163e5d9d3b5d040987e4dfcacad4571cc8
Paweł Miążek <pawel@linacti.pl>
2020-10-22 15:56:23 +0200

    Added backend Django and frontend ReactJS

---
+++ backend/backend/env.py
@@ -0,0 +1,11 @@
+ALLOWED_HOSTS = ['localhost']
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'recipe',
+        'USER': 'recipe',
+        'PASSWORD': 'recipe',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
